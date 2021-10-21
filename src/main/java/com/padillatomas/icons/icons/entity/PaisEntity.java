f30375222c9b292252f1dd0b3093a666a0bf6615
Tomas Padilla <padillatomasagustin@gmail.com>
2021-10-21 17:08:24 +0200

    Creamos Projecto MAMP 8889 icons, Poblamos Database con Entidades

---
+++ src/main/java/com/padillatomas/icons/icons/entity/PaisEntity.java
@@ -0,0 +1,75 @@
+package com.padillatomas.icons.icons.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pais")
+@Getter
+@Setter
+public class PaisEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	private String imagen;
+	private String denominacion;
+	
+	@Column(name = "cant_habitantes")
+	private Long cantidadHabitantes;
+	
+	private Long superficie; // m2
+	
+	// *** Para busca Informacion (Dentro del Objeto ContinenteEntity) y armar Listas ***
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "continente_id", insertable = false, updatable = false)
+	private ContinenteEntity continente;
+	
+	// *** Para Updatear, Guardar y Actualizar ***
+	@Column(name = "continente_id", nullable = false)
+	private Long continenteId;
+	
+	
+	// *** 
+	// Cuando creamos Pais, podemos pasarle Lista de Iconos
+	// Creamos Tabla Intermedia "icon_pais"
+	//***
+	@ManyToMany(
+			cascade = {
+					CascadeType.PERSIST,
+					CascadeType.MERGE,
+			})
+	@JoinTable(
+			name = "icon_pais",
+			joinColumns= @JoinColumn(name = "pais_id"),
+			inverseJoinColumns = @JoinColumn(name = "icon_id"))
+	private Set<IconEntity> icons = new HashSet<>();
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null)
+			return false;
+		if(getClass() != obj.getClass())
+			return false;
+		final PaisEntity other = (PaisEntity) obj;
+		return other.id ==this.id;
+	}
+	
+}
